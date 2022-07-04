efc10f22f5057e6410523eed029b453691d4fb38
dr1co <dricomatoba@hotmail.com>
2022-07-04 16:32:08 -0300

    using cors only

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const server = express();
 const PORT = process.env.PORT;
 
 server.use(cors());
-server.use((req, res, next) => {
+/* server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With", "Content-Type", "Accept");
     next();
-});
+}); */
 server.use(express.json());
 
 server.use(authRouter);
