56af3f1f0c8cca9dcf49913ce6821baa244cf7bb
dr1co <dricomatoba@hotmail.com>
2022-07-04 16:07:54 -0300

    use(cors()) before header + commited .env archive

--- src/index.js
+++ src/index.js
@@ -7,11 +7,11 @@ import transactionsRouter from './routes/transactionsRoutes.js';
 const server = express();
 const PORT = process.env.PORT;
 
+server.use(cors());
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With", "Content-Type", "Accept");
-    server.use(cors());
     next();
 });
 server.use(express.json());
